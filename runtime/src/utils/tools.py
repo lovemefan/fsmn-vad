76c7698ed938b6627ea9ffd594e4191e90542241
lovemefan <lovemefan@outlook.com>
2023-04-04 02:47:37 +0800

    add tools

---
+++ runtime/src/utils/tools.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+# @FileName  :tools.py
+# @Time      :2023/4/3 19:17
+# @Author    :lovemefan
+# @Email     :lovemefan@outlook.com
+from pathlib import Path
+from typing import Union, Dict
+
+import yaml
+
+
+def read_yaml(yaml_path: Union[str, Path]) -> Dict:
+    if not Path(yaml_path).exists():
+        raise FileExistsError(f'The {yaml_path} does not exist.')
+
+    with open(str(yaml_path), 'rb') as f:
+        data = yaml.load(f, Loader=yaml.Loader)
+    return data
